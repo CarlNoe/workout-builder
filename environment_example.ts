5d8da87a641bc2a7619d60551fbf97bda118e7f9
Noe Carl <noe.carldev@gmail.com>
2023-01-04 13:59:03 +0100

    Externalised api key

---
+++ environment_example.ts
@@ -0,0 +1,7 @@
+// Rename this file to environment.ts
+// Add the API_KEY and API_HOST to this file
+
+export const environment = {
+  API_KEY: 'API_KEY',
+  API_HOST: 'API_HOST',
+};
--- src/app/services/exercises-api.service.ts
+++ src/app/services/exercises-api.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'environment';
 
 const options = {
   headers: new HttpHeaders({
-    'X-RapidAPI-Key': 'cb8e053bc7mshbdf5473241a50b2p14f2efjsn98533256bb9e',
-    'X-RapidAPI-Host': 'exercises2.p.rapidapi.com',
+    'X-RapidAPI-Key': environment.API_KEY,
+    'X-RapidAPI-Host': environment.API_HOST,
   }),
 };
 
