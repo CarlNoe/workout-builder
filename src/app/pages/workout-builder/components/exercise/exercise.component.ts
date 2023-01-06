fdc6c3d57d0a51b60a72ae7e7d50f7d5cf9ebc18
Noe Carl <noe.carldev@gmail.com>
2023-01-06 16:59:21 +0100

    Delete exercise added

--- src/app/pages/workout-builder/components/exercise/exercise.component.ts
+++ src/app/pages/workout-builder/components/exercise/exercise.component.ts
@@ -16,4 +16,14 @@ export class ExerciseComponent {
     repAmount: 0,
     setAmount: 0,
   };
+
+  deleteWorkoutExerciseFromLocalStorage() {
+    let exercises = JSON.parse(localStorage.getItem('exercises') || '[]');
+
+    exercises = exercises.filter(
+      (exercise: WorkoutExercise) => exercise.id !== this.workoutExercise.id
+    );
+
+    localStorage.setItem('exercises', JSON.stringify(exercises));
+  }
 }
