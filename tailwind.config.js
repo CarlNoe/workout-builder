0f807f573244144421d553bbcc30973b5d28ac9e
Noe Carl <noe.carldev@gmail.com>
2023-01-07 01:27:15 +0100

    Operation CBUM Succesful.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
         main: ["Bebas Neue", "sans-serif"],
         secondary: ["Raleway", "sans-serif"],
       },
+      screens: {
+        "2xl": "1440px",
+        "3xl": "1920px",
+      },
     },
     plugins: [],
   }
